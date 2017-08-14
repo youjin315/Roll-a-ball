bb0adaa09caa3acd29eac9c501ab4f8bd3104397
Elizabeth Youn <eli.youjin315@gmail.com>
2017-08-14 18:39:09 -0400

    Add unity files

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    private int count;
+
+    public Text countText;
+
+    public Text winText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * speed); 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            Destroy(other.gameObject);
+
+            count++;
+
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "You win!";
+        }
+    }
+}
+/*if I want walls to be obstacle:
+ * 
+ private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        SetInstructionText();
+    }
+ 
+ void OnTriggerEnter(Collider other)
+        if (other.gameObject.CompareTag("Wall"))
+        SetEndText();
+        (and EndText=You lose!)*/
+
---
+++ Assets/Script/Spawn.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+
+    public GameObject pickUp;
+
+    public float interval = 2.0f;
+
+    public float destroyInterval = 5.0f;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        //Run the SpawnPickup function without blocking the game
+        StartCoroutine(SpawnPickup());
+    }
+
+    private IEnumerator SpawnPickup()
+    {
+        // Run this loop forever 
+        while (true)
+        {
+            //Wait for interval seconds
+            yield return new WaitForSeconds(interval);
+
+            //Determine the minimal position for the pickup to spawn
+            Vector3 min = transform.position - transform.localScale / 2f;
+
+            //Determine the maximal posiion for the spawn
+            Vector3 max = transform.position + transform.localScale / 2f;
+
+            //Fix the Y axis (not 0, 0.5)
+            //For x and z axis, it is a random number between (-10 and 10)
+            Vector3 position = new Vector3(Random.Range(min.x, max.x), transform.position.y, Random.Range(min.z, max.z));
+
+            //Creat pickup object at position we just calculated
+            GameObject newPickup = Instantiate(pickUp, position, pickUp.transform.rotation);
+
+            //Destroy the newPickUp if not collected by the player within destroyInterval
+            Destroy(newPickup, destroyInterval);
+        }
+
+        
+
+    }
+}
